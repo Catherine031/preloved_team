2dbde93657f0916fbf647fda898515686461db9f
Chizoba <chizobaegwu22@gmail.com>
2025-05-30 21:44:37 +0200

    UI Changes

--- PreLoved/app/src/main/java/com/example/preloved/LoginActivity.java
+++ PreLoved/app/src/main/java/com/example/preloved/LoginActivity.java
@@ -3,8 +3,10 @@ package com.example.preloved;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +22,7 @@ import org.json.JSONObject;
 public class LoginActivity extends AppCompatActivity {
     EditText editEmail, editPassword;
     Button btnLogin, btnGoToRegister;
+    TextView txtCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
         editEmail = findViewById(R.id.editEmail);
         editPassword = findViewById(R.id.editPassword);
         btnLogin = findViewById(R.id.btnLogin);
-        btnGoToRegister = findViewById(R.id.btnGoToRegister);
+        txtCreateAccount = findViewById(R.id.txtCreateAccount);
 
         btnLogin.setOnClickListener(v -> {
             String email = editEmail.getText().toString().trim();
@@ -92,7 +95,7 @@ public class LoginActivity extends AppCompatActivity {
             queue.add(stringRequest);
         });
 
-        btnGoToRegister.setOnClickListener(v -> {
+        txtCreateAccount.setOnClickListener(v -> {
             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
             startActivity(intent);
         });
